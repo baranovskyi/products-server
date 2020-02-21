68ade9c992a6aae17b4df62487997e2f6c230fd2
Baranovskyi Oleksandr <obaranovskyi@exadel.net>
2020-02-21 11:37:11 +0200

    .

---
+++ index.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+const databaseUrl =
+  "mongodb+srv://admin:admin@cluster0-30ljv.mongodb.net/test?retryWrites=true&w=majority";
+let db;
+MongoClient.connect(
+  databaseUrl,
+  { useUnifiedTopology: true },
+  (err, client) => {
+    if (err) return console.log(err);
+    db = client.db("storeProducts");
+    app.listen(3000, () => {
+      console.log("database is on port 3000");
+    });
+  }
+);
+app.get("/", (req, res) => {
+  res.send("hello from mongoDB");
+});
+app.all("/*", function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization, Content-Length, X-Requested-With, "
+  );
+  res.header({
+    "Content-type": "application/json"
+  });
+  next();
+});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+app.get("/all-products", (req, res) => {
+  db.collection("products")
+    .find()
+    .toArray((err, result) => {
+      if (err) console.log("cannot get products", err);
+      res.json(result);
+    });
+});
+app.get("/productId", (req, res) => {
+  let id = req.query._id;
+  db.collection("products")
+    .find({ _id: ObjectId(id) })
+    .toArray((err, result) => {
+      res.json(result[0]);
+    });
+});
+app.post("/addProduct", (req, res) => {
+  console.log(req.body);
+  db.collection("products").save(req.body, (err, result) => {
+    if (err) console.error(err);
+    res.send(result);
+  });
+});
+app.put("/buyProducts", (req, res) => {
+  let id = req.body._id;
+  db.collection("products").findOneAndUpdate(
+    { _id: ObjectId(id) },
+    {
+      $set: {
+        buy: "true"
+      }
+    },
+    (err, result) => {
+      if (!req.body._id) return res.send(err);
+      res.send(result);
+    }
+  );
+});
+app.get("/cardProducts", (req, result) => {
+  db.collection("products")
+    .find({ buy: "true" })
+    .toArray((err, res) => {
+      result.json(res);
+    });
+});
